709ef6af93f376a6a0ac2f391baf6a6ee4a88fff
amyfranses <amyfranses@gmail.com>
2022-06-27 16:49:01 +0000

    ex1: create 3 stores in DB, ex2: update store name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+store = Store.find_by(id: 1)
+@store1 = store.id 
+
+store = Store.find_by(id: 2)
+@store2 = store.id 
+
+store = Store.find_by(id: 1)
+store.update(name: "Toronto")
