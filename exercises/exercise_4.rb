417abfacd05ff3eeb72c9b52b31cf0bbe63be37a
amyfranses <amyfranses@gmail.com>
2022-06-27 18:11:37 +0000

    create new stores, use where method to access specific criteria

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,13 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store4 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+store5 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+store6 = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+puts Store.count
+
+# pp Store.where(mens_apparel: true)
+
+pp Store.where("annual_revenue < 1000000", womens_apparel: true)
+
