8e33df73734c0826dbab5912379aa7e5ab5f3822
amyfranses <amyfranses@gmail.com>
2022-06-28 18:04:23 +0000

    update ex7 with custom validation function

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,27 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee <  ActiveRecord::Base
+  validates :first_name, :last_name, presence: true
+  validates :hourly_rate, numericality: {only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200}
+  validates :store_id, numericality: {only_integer: true}
+end
+
+class Store <  ActiveRecord::Base
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: {only_integer: true, greater_than: 0}
+  validate :validate_apparel_type
+  def validate_apparel_type
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, "You cannot create a store without men's or women's apparel")
+  end
+end
+  # validate :apparel_type, mens_apparel: true
+  #if !, inclusion: [true, false]
+end
+
+puts "Please add a store"
+store_name = gets.chomp
+
+store = Store.create(name: store_name)
+p store.errors.full_messages
